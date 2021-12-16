f90979a80f23ebe4488c8cdd30effe09ec4dbbf6
neetrophy <sharma1992nee@gmail.com>
2021-12-16 16:09:45 +0000

    fixed issue with list and dict

--- MSTeamsdataretrieval/TeamsDataRetrival.py
+++ MSTeamsdataretrieval/TeamsDataRetrival.py
@@ -38,10 +38,13 @@ class MSTeamsAttendanceProcessor:
     def getAttendeesDetailsAsDict(self):
         Msdict = {}
         MsList = []
+        MsListData = []
         val = ""
+        key = ""
         fileCsvlines = self.splitFilesByNewline()
         self.setMeetingDetails()
         for fileData in fileCsvlines: 
+            MsList = []
             if len(fileData.split('\t')) == 7 and 'Full Name' not in fileData:
                 for i in range(0,6):
                     #converting time to minutes
@@ -53,15 +56,17 @@ class MSTeamsAttendanceProcessor:
                 MsList.append(self.meetingTitle)
                 MsList.append(self.meetingId)
                 MsList.append(self.organiser)
-                Msdict[fileData.split('\t')[4]] = MsList
+                key = fileData.split('\t')[4]
+                Msdict[key] = MsList
         return Msdict
     #returns the details of the attendees as a list with  details of the attendees
     #organises the data which will later be used for calculations and displaying data in the dashboard
     def getAttendeesDetailsAsList(self):
-        MsList = []
+        MsListData = []
         fileCsvlines = self.splitFilesByNewline()
         self.setMeetingDetails()
         for fileData in fileCsvlines: 
+            MsList = []
             if len(fileData.split('\t')) == 7 and 'Full Name' not in fileData:
                 for i in range(0,6):
                     #converting time to minutes
@@ -73,7 +78,8 @@ class MSTeamsAttendanceProcessor:
                 MsList.append(self.meetingTitle)
                 MsList.append(self.meetingId)
                 MsList.append(self.organiser)
-        return MsList
+                MsListData.append(MsList)
+        return MsListData
 
     #returns a list of data for master data model
     def getAttendeesforMasterData(self):
@@ -86,3 +92,10 @@ class MSTeamsAttendanceProcessor:
         return MsList
 
 
+someString = "Meeting Summary \nTotal Number of Participants	2\nMeeting Title	Devops project \nMeeting Start Time	12/10/2021, 7:37:51 PM\nMeeting End Time	12/10/2021, 8:07:14 PM\nMeeting Id	453adbad-3672-4bde-92d0-6be3049eb379\nNeeti Sharma	12/10/2021, 7:37:51 PM	12/10/2021, 8:07:14 PM	29m 22s	x20242778@student.ncirl.ie	Organiser	x20242778@student.ncirl.ie \n Parag Sharad Salunkhe	12/10/2021, 7:38:13 PM	12/10/2021, 8:07:12 PM	28m 58s	x20224699@student.ncirl.ie	Presenter	x20224699@student.ncirl.ie"
+
+TeamsProc = MSTeamsAttendanceProcessor(someString) 
+#print(TeamsProc.splitFilesByNewline())
+output = TeamsProc.getAttendeesDetailsAsDict()
+print(output)
+print(type(TeamsProc.splitFilesByNewline()))
